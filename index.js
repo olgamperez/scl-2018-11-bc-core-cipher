523b8b89871fccc9e68b483046784080104bd835
olgamperez <ing.olgaperez@gmail.com>
2018-11-29 12:10:12 -0300

    Listo formula del cifrado mayusculas y minisculas

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-/* Acá va tu código */
+//aqui solo va el manejo del DOM
+window.onload =() => {
+    const btnCifrar = document.getElementById("cifrar");//identificando el boton con una variable
+    btnCifrar.addEventListener("click", ()=>{ //Asignandole una funcion al boton
+        let menssage1 = document.getElementById("mensaje").value; //evaluando lo que se encuentra dentro de mi caja de usuario
+        let desplazamiento1 = document.getElementById("desplazamiento").value;
+        document.getElementById("resuelto").innerHTML= window.cipher.encode(desplazamiento1,menssage1); //Indicandole que escriba en mi textarea el resultado del cifrar
+        return window.cipher.encode(desplazamiento1,menssage1)//Aqui estoy retornando el valor del texto codificado
+
+    })
+    const btnDescifrar = document.getElementById("descifrar");
+    btnDescifrar.addEventListener("click", ()=>{ 
+        let menssage1 = document.getElementById("mensaje").value; 
+        let desplazamiento1 = document.getElementById("desplazamiento").value;
+        document.getElementById("resuelto").innerHTML= window.cipher.decode(desplazamiento1,menssage1);  
+        return window.cipher.decode(desplazamiento1,menssage1)
+})
+}
