0e06d407a62485cad751946dc6ae52fb86b91f9a
olgamperez <ing.olgaperez@gmail.com>
2018-12-03 07:54:48 -0300

    Se agrego contenido a la formula cipher y mejorando el diseño

--- src/cipher.js
+++ src/cipher.js
@@ -12,6 +12,13 @@ window.cipher = {
       }else if (letraAscii >= 97 && letraAscii <= 122 ) {
         let letraConvertida=(letraAscii-97+offset)%26+97; 
         resultado += String.fromCharCode(letraConvertida);
+      }
+      else if (letraAscii >= 48 && letraAscii <= 57 ) {
+        let letraConvertida=(letraAscii-48+offset)%10+48; 
+        resultado += String.fromCharCode(letraConvertida);
+      }else if (letraAscii >= 164 && letraAscii <= 165 ) {
+          continue; //omite la letra "ñÑ"
+          resultado += String.fromCharCode(letraConvertida);
       }else{
         resultado += texto[i]
       }  
@@ -27,8 +34,7 @@ window.cipher = {
     if (letraAscii >= 65 && letraAscii <= 90 ) {
       let letraConvertida=(letraAscii-65-offset)%26+65; 
       if(letraConvertida <65){
-        (letraConvertida+=26)
-      };
+        (letraConvertida+=26)};
       resultado += String.fromCharCode(letraConvertida);
     }else if (letraAscii >= 97 && letraAscii <= 122 ) {
       let letraConvertida=(letraAscii-97-offset)%26+97; 
@@ -36,10 +42,15 @@ window.cipher = {
         (letraConvertida+=26)
       };
       resultado += String.fromCharCode(letraConvertida);
-    }else{
-      resultado += texto[i]
-    }
-    
-    } 
-    return resultado;
+    }else if (letraAscii >= 48 && letraAscii <= 57 ) {
+      let letraConvertida=(letraAscii-48-offset)%10+48; 
+      if(letraConvertida <48){
+        (letraConvertida +=10)};
+      resultado += String.fromCharCode(letraConvertida);
+    }else if (letraAscii >= 164 && letraAscii <= 165 ) {
+      continue; 
+      resultado += String.fromCharCode(letraConvertida);
+    } else{
+      resultado += texto[i]}
+    } return resultado;
   }}
